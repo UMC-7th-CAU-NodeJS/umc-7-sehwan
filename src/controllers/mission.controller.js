28518e7835b820424545adae7bc8ca7635306475
sehwanii_mac <sehwan1023@gmail.com>
2024-11-08 15:16:57 +0900

    feat : fetch store missions

--- src/controllers/mission.controller.js
+++ src/controllers/mission.controller.js
@@ -1,6 +1,10 @@
 import { StatusCodes } from "http-status-codes";
 import { bodyToMission, challengeMissionDTO } from "../dtos/mission.dto.js";
-import { createMission,challengeMission } from "../services/mission.service.js";
+import {
+  createMission,
+  challengeMission,
+  storeGetMission,
+} from "../services/mission.service.js";
 
 export const handleCreateMission = async (req, res, next) => {
   console.log("미션 추가를 요청했습니다!");
@@ -8,11 +12,11 @@ export const handleCreateMission = async (req, res, next) => {
 
   const user = await createMission(bodyToMission(req.body));
   res.status(StatusCodes.OK).json({ result: user });
-}
+};
 
 export const handleChallengeMission = async (req, res) => {
   try {
-    const { storeId, missionId } = req.params;  // 경로 파라미터에서 storeId와 missionId 가져오기
+    const { storeId, missionId } = req.params; // 경로 파라미터에서 storeId와 missionId 가져오기
 
     // DTO 함수로 요청 데이터를 처리 (경로 파라미터와 요청 본문 전달)
     const dto = challengeMissionDTO(storeId, missionId, req.body);
@@ -29,4 +33,24 @@ export const handleChallengeMission = async (req, res) => {
     // 오류 응답
     res.status(400).json({ error: error.message });
   }
+};
+
+export const handleStoreMission = async (req, res, next) => {
+    console.log("미션 조회를 요청했습니다!");
+    console.log("body:", req.body); // 값이 잘 들어오나 확인하기 위한 테스트용
+    try {
+    const { storeId } = req.params; // 경로 파라미터에서 storeId와 missionId 가져오기
+
+    // 서비스 레이어 호출, DTO 데이터를 전달
+    const storeMissionId = await storeGetMission(storeId);
+
+    // 성공 응답
+    res.status(201).json({
+      message: "미션에 성공적으로 도전했습니다.",
+      storeMissionId,
+    });
+  } catch (error) {
+    // 오류 응답
+    res.status(400).json({ error: error.message });
+  };
 };
